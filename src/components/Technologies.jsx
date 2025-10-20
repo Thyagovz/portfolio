b2e591594f631033437709fd1c4dca1fd249ebc1
Thyago Euclides <flooddk@gmail.com>
2025-10-20 19:37:11 -0400

    Adds UI animations with Framer Motion
    Integrates Framer Motion to enhance the user interface with dynamic animations.
    
    - Implements scroll-triggered fade-in and slide-in effects for sections such as Contact, Experiences, Projects, and Technologies.
    - Introduces staggered animations for the Hero section's textual content and image.
    - Adds continuous floating animations for the technology icons.
    - Updates ESLint configuration to ignore `motion` variables, preventing linting errors.
    - Enhances the `TE-logo.svg` by adding a new central horizontal stroke.
    - Translates "Experience" and "Projects" section titles to Portuguese for improved localization.

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -3,40 +3,110 @@ import { FaCss3Alt, FaHtml5, FaNodeJs } from "react-icons/fa";
 import { RiJavascriptLine, RiReactjsLine } from "react-icons/ri";
 import { SiTailwindcss } from "react-icons/si";
 import { TbBrandNextjs, TbBrandTypescript } from "react-icons/tb";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
   return (
     <div className="pb-24">
-      <h2 className="my-20 text-center text-4xl">Tecnologias</h2>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Tecnologias
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(2.5)}
+        >
           <RiReactjsLine className="text-7xl text-cyan-400" />
-        </div>
-        <div className="p-4">
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(3)}
+          className="p-4"
+        >
           <TbBrandNextjs className="text-7xl" />
-        </div>
-        <div className="p-4">
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(5)}
+          className="p-4"
+        >
           <BiLogoPostgresql className="text-7xl text-sky-700" />
-        </div>
-        <div className="p-4">
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(2)}
+          className="p-4"
+        >
           <SiTailwindcss className="text-7xl text-cyan-500" />
-        </div>
-        <div className="p-4">
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(6)}
+          className="p-4"
+        >
           <FaNodeJs className="text-7xl text-green-500" />
-        </div>
-        <div className="p-4">
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(4)}
+          className="p-4"
+        >
           <RiJavascriptLine className="text-7xl text-yellow-300" />
-        </div>
-        <div className="p-4">
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(3)}
+          className="p-4"
+        >
           <FaHtml5 className="text-7xl text-orange-600" />
-        </div>
-        <div className="p-4">
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(5)}
+          className="p-4"
+        >
           <FaCss3Alt className="text-7xl text-blue-500" />
-        </div>
-        <div className="p-4">
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(2)}
+          className="p-4"
+        >
           <TbBrandTypescript className="text-7xl text-blue-400" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
