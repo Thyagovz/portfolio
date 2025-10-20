049de8dffa17cf7507f40b4d64cc4fefcc7aadc5
Thyago Euclides <flooddk@gmail.com>
2025-10-20 16:22:40 -0400

    Adds Hero section to landing page
    Implements the main introductory section, showcasing personal details, a profile image, and a call-to-action for CV download. This completes the initial structure for the landing page.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <a href="/" aria-label="Home">
-          <img src={logo} className="mx-2" width={80} height={33} />
+          <img src={logo} className="justify-start-2" width={80} height={33} />
         </a>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
