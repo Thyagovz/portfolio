c27643c4d3a53613ef9e735aae5403eeccc8316a
Thyago Euclides <flooddk@gmail.com>
2025-10-20 18:05:23 -0400

    Adds contact section
    Integrates the new Contact component into the main application layout. This provides a dedicated section for users to get in touch, enhancing the application's completeness.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Contact from "./components/Contact";
 import Experiences from "./components/Experiences";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -20,6 +21,7 @@ const App = () => {
         <Technologies />
         <Projects />
         <Experiences />
+        <Contact />
       </div>
     </div>
   );
